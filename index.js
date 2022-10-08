ca1c8ccee88ab036ef45c7bf9be40fc2ddda202b
Eunice Souza <ejs.12@hotmail.com>
2022-10-07 23:24:20 -0300

    Alguns probleminhas para ser resolvidos

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu() {
+    if(event.type === 'touchStart') event.preventDefault()
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', 'true')
+    if (active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    }else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
+
 }
-btnMobile.addEventListener('clik', toggleMenu);
+btnMobile.addEventListener('Clik', toggleMenu)
+btnMobile.addEventListener('touchsTart', toggleMenu)
